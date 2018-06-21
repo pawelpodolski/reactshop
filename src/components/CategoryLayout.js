e1d04030c15482f7f0e321c8df169f5d66df4914
Piotr Staniów <staniowp@gmail.com>
2018-06-21 23:26:26 +0200

    Exercise 2: Writing components

--- src/components/CategoryLayout.js
+++ src/components/CategoryLayout.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import CategoryGrid from "./CategoryGrid";
+import CategoryDetails from "./CategoryDetails";
 
 class CategoryLayout extends React.Component {
     categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4'];
+    category = {
+        "id": "agd",
+        "productIds": ["item1000", "item1001", "item1002"]
+    };
+
     render() {
         return (
-            <CategoryGrid categories={this.categories} />
+            <React.Fragment>
+                <CategoryGrid categories={this.categories}/>
+                <CategoryDetails category={this.category}/>
+            </React.Fragment>
         );
     }
 }
